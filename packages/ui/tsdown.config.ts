e6d9e7b678dc930410708a8fac7674f32eb8aa6f
sxy15 <sxy_19921115@163.com>
2025-11-21 10:24:14 +0800

    ci: update

--- packages/ui/tsdown.config.ts
+++ packages/ui/tsdown.config.ts
@@ -12,10 +12,4 @@ export default defineConfig(() => ({
     dts: {
         vue: true,
     },
-    resolve: {
-        alias: {
-            '@zzu/use': '../use',
-            '@zzu/utils': '../utils',
-        },
-    },
 }));
