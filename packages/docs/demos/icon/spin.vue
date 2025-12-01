1dd17947ed241a78685d94eed721360fbd3c320c
sxy15 <sxy_19921115@163.com>
2025-12-01 16:14:35 +0800

    feat: update icon

--- packages/docs/demos/icon/spin.vue
+++ packages/docs/demos/icon/spin.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="icon-container">
-        <IconRotateCw size="24" spin stroke-width="2" />
+        <IconLoading size="24" spin stroke-width="2" />
     </div>
 </template>
 
 <script setup lang="ts">
-import { IconRotateCw } from '@zzu/icon';
+import { IconLoading } from '@zzu/icon';
 
 defineOptions({
     name: 'DocZzuIconSpin',
     components: {
-        IconRotateCw,
+        IconLoading,
     },
 });
 </script>
